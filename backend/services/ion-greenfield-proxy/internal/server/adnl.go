665676c522bf79c6e81eb36d9545ee4f71adafd4
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2026-04-07 17:07:12 +0200

    ion-greenfield-proxy: fix context usage and add more logs

--- backend/services/ion-greenfield-proxy/internal/server/adnl.go
+++ backend/services/ion-greenfield-proxy/internal/server/adnl.go
@@ -13,9 +13,9 @@ func NewADNLServer(listener *adnl.Listener, logger *slog.Logger, lc fx.Lifecycle
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			logger.Info("adnl server starting")
-			return listener.Start(ctx)
+			return listener.Start(context.WithoutCancel(ctx))
 		},
-		OnStop: func(_ context.Context) error {
+		OnStop: func(context.Context) error {
 			logger.Info("adnl server stopping")
 			return listener.Stop()
 		},
