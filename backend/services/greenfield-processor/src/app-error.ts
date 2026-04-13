538ec5f5fb742a9d2e212253c780fef396798a0a
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2026-04-13 12:46:45 +0300

    comments

--- backend/services/greenfield-processor/src/app-error.ts
+++ backend/services/greenfield-processor/src/app-error.ts
@@ -1,10 +1,12 @@
 export default class AppError extends Error {
   readonly statusCode: number;
+  readonly errorCode: string;
 
-  constructor(message: string, statusCode = 500) {
+  constructor(message: string, statusCode = 500, errorCode = 'UNKNOWN_ERROR') {
     super(message);
     this.name = 'AppError';
     this.statusCode = statusCode;
+    this.errorCode = errorCode;
     Error.captureStackTrace?.(this, AppError);
   }
 }
